527bb5152706ea79f662371a2d5b09d4251841a3
nitaifingerhut <nitaifingerhut@gmail.com>
2021-09-13 13:02:35 +0300

    scripts

--- scripts/train_rl.sh
+++ scripts/train_rl.sh
@@ -8,5 +8,4 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-python train_mle.py --mode v1 --train-steps 10000 --valid-steps 1000 --report-every 100 --save-every 1000
-# python train_mle.py --mode v2
+python train_rl.py --mode v1 --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100
