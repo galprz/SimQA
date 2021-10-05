5278992b49b2a34e60f80dc18ae0fe1450e224bf
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-10-05 09:07:39 +0300

    scripts

---
+++ scripts/train_mle_00.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setup env
+cd ..
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate SimQA-code
+echo "hello from $(python --version) in $(which python)"
+echo "running train_mle.py experiments"
+
+# Run the experiments
+python train_mle.py --mode v1 --valid-steps 1000 --report-every 100 --name baseline
+python train_mle.py --mode v2 --valid-steps 1000 --report-every 100 --name baseline
---
+++ scripts/train_mle_03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setup env
+cd ..
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate SimQA-code
+echo "hello from $(python --version) in $(which python)"
+echo "running train_mle.py experiments"
+
+# Run the experiments
+python train_mle.py --mode v2 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 05 --lookahead-alpha 1.00 --name lookahead_ls_05_la_1.00
+python train_mle.py --mode v2 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 10 --lookahead-alpha 1.00 --name lookahead_ls_10_la_1.00
+python train_mle.py --mode v2 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 15 --lookahead-alpha 1.00 --name lookahead_ls_15_la_1.00
