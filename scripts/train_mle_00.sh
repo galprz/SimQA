8e2a7f45e68d95090fbe47ad34415540749fddab
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-10-13 16:34:47 +0300

    .

--- scripts/train_mle_00.sh
+++ scripts/train_mle_00.sh
@@ -8,6 +8,6 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-#python train_mle.py --mode v1 --valid-steps 1000 --report-every 100 --name baseline
-#python train_mle.py --mode v2 --valid-steps 1000 --report-every 100 --name baseline
-python train_mle.py --mode v3 --valid-steps 1000 --report-every 100 --name baseline
+#python train_mle.py --version v1 --valid-steps 1000 --report-every 100 --name baseline
+#python train_mle.py --version v2 --valid-steps 1000 --report-every 100 --name baseline
+python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --name baseline
--- scripts/train_mle_04.sh
+++ scripts/train_mle_04.sh
@@ -8,6 +8,6 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-python train_mle.py --mode v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 05 --lookahead-alpha 10.0 --name lookahead_ls_05_la_10.0
-python train_mle.py --mode v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 10 --lookahead-alpha 10.0 --name lookahead_ls_10_la_10.0
-python train_mle.py --mode v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 15 --lookahead-alpha 10.0 --name lookahead_ls_15_la_10.0
+python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 05 --lookahead-alpha 10.0 --name lookahead_ls_05_la_10.0
+python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 10 --lookahead-alpha 10.0 --name lookahead_ls_10_la_10.0
+python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 15 --lookahead-alpha 10.0 --name lookahead_ls_15_la_10.0
