d29e8a75e93cf01ca75e21b5acc2db4c66865167
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-10-13 17:58:48 +0300

    .

--- scripts/train_mle_00.sh
+++ scripts/train_mle_00.sh
@@ -8,6 +8,6 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-#python train_mle.py --version v1 --valid-steps 1000 --report-every 100 --name baseline
-#python train_mle.py --version v2 --valid-steps 1000 --report-every 100 --name baseline
-python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --name baseline
+#python train_mle.py --version v1 --valid-steps 1000 --report-every 100 --train-batch-size 24 --name baseline
+#python train_mle.py --version v2 --valid-steps 1000 --report-every 100 --train-batch-size 24 --name baseline
+python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --train-batch-size 24 --name baseline
--- scripts/train_mle_03.sh
+++ scripts/train_mle_03.sh
@@ -8,6 +8,6 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 05 --lookahead-alpha 1.00 --name lookahead_ls_05_la_1.00
-python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 10 --lookahead-alpha 1.00 --name lookahead_ls_10_la_1.00
-python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 15 --lookahead-alpha 1.00 --name lookahead_ls_15_la_1.00
+python train_mle.py --version v3 --train-batch-size 24 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 05 --lookahead-alpha 1.00 --name lookahead_ls_05_la_1.00
+python train_mle.py --version v3 --train-batch-size 24 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 10 --lookahead-alpha 1.00 --name lookahead_ls_10_la_1.00
+python train_mle.py --version v3 --train-batch-size 24 --valid-steps 1000 --report-every 100 --lookahead --lookahead-steps 15 --lookahead-alpha 1.00 --name lookahead_ls_15_la_1.00
