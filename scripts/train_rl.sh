5933114b974fddc5834ea95ee7dd297de45f3092
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-10-23 11:52:06 +0300

    .

--- scripts/train_rl.sh
+++ scripts/train_rl.sh
@@ -9,4 +9,4 @@ echo "running train_mle.py experiments"
 
 # Run the experiments
 python train_rl.py --version v3 --model-path checkpoints/MLE/v3/baseline_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --name baseline
-python train_rl.py --version v3 --model-path checkpoints/MLE/v3/lookahead_ls_15_la_1.00_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --name lookahead
+#python train_rl.py --version v3 --model-path checkpoints/MLE/v3/lookahead_ls_15_la_1.00_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --name lookahead
