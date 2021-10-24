243694864d92b66ff11d3332c37b8474c7503723
nitaifingerhut <nitaifingerhut@gmail.com>
2021-10-24 14:07:10 +0300

    .

--- scripts/train_both.sh
+++ scripts/train_both.sh
@@ -8,5 +8,5 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-python train_mle.py --version v3 --valid-steps 1000 --report-every 100 --train-batch-size 24 --name baseline
-python train_rl.py --version v3 --model-path checkpoints/MLE/v3/baseline_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --name baseline
+#python train_mle.py --version v2 --valid-steps 1000 --report-every 100 --train-batch-size 24 --name baseline
+python train_rl.py --version v2 --model-path checkpoints/MLE/v2/baseline_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --name baseline
