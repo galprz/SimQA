5d413c07da3f592824a55589339d83439397909e
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2021-11-09 11:28:02 +0200

    augment return only

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,4 +8,5 @@ echo "hello from $(python --version) in $(which python)"
 echo "running train_mle.py experiments"
 
 # Run the experiments
-python train_rl.py --train-version v2 --valid-version v3 --model-path checkpoints/MLE/v2/baseline_step_10000.pt --train-steps 1000 --valid-steps 100 --report-every 100 --save-every 100 --reward-gamma 0.5 --name baseline
+python train_rl.py --train-version v2 --valid-version v3 --model-path checkpoints/MLE/v2/baseline_step_10000.pt --train-steps 1500 --valid-steps 100 --report-every 100 --save-every 100 --reward-gamma 0.5 --name train_on_v2
+python train_rl.py --train-version v3 --valid-version v3 --model-path checkpoints/MLE/v2/baseline_step_10000.pt --train-steps 1500 --valid-steps 100 --report-every 100 --save-every 100 --reward-gamma 0.5 --name train_on_v3
