e9ab1b1f53dd4a4cdfe1f33727e27cd7c792777c
arrafmousa <arraf46@gmail.com>
2022-12-08 14:27:14 +0200

    debug output

--- metrics.py
+++ metrics.py
@@ -52,6 +52,7 @@ class MSEScore(Metric):
             try:
                 answer, _ = execute_simcode(target_code, True)
                 pred_answer, _ = execute_simcode(pred_code, True)
+                print("appending "+str((answer-pred_answer)**2))
                 np.append(self.errors, [(answer-pred_answer)**2])
             except Exception as e:
                 # print(e)
